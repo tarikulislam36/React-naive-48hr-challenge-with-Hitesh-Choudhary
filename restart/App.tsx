090cd27fccc9e81adb7e13c29419622688c7c894
tarikulislam <tarikulislam4145@gmail.com>
2024-04-15 17:54:56 +0530

    organize file and create separate component folder

--- restart/App.tsx
+++ restart/App.tsx
@@ -1,11 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+import Flatcard from './Component/Flatcard'
 
 const App = () => {
   return (
-    <View>
-      <Text>Rstart</Text>
-    </View>
+    
+    <SafeAreaView> 
+      <ScrollView>
+
+   <Flatcard />
+    </ScrollView>
+    </SafeAreaView>
   )
 }
 
---
+++ restart/Component/Flatcard.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Flatcard() {
+  return (
+    <View>
+      <Text>Flatcard</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
